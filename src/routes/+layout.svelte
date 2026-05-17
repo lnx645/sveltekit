6324c572adeda6154f49c206de26ee08bef944a9
lnx645 <dadanhidyt@gmail.com>
2026-05-17 21:29:55 +0700

    update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+<nav>
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam ad fuga, fugit sunt aliquam neque magni suscipit quas! Totam aliquam magnam doloremque, id aut nihil corporis inventore consequuntur recusandae. Eius.
+</nav>
+{@render children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import axios from "axios";
+	import { onMount } from "svelte";
+
+	onMount(()=>{
+		axios.get("/login")
+	})
+</script>
+
+<h2>Selamat datang di invitnesia.com</h2>
